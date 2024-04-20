1a91d751ed68fca2209f853a0b9905a4a5c111f1
adcohen1 <adcohen@uninorte.edu.co>
2024-04-20 17:52:02 -0500

    SSprites añadidos

--- Clases.py
+++ Clases.py
@@ -1,5 +1,5 @@
 import pygame as pg
-
+import sys
 import constantes as c
 
 
--- Vista.py
+++ Vista.py
@@ -8,7 +8,6 @@ pg.display.set_caption('El Parqueadero')
 piso1 = pg.image.load('piso1.png').convert_alpha()
 piso2 = pg.image.load('piso2.png').convert_alpha()
 piso3 = pg.image.load('piso3.png').convert_alpha()
-panel = pg.image.load('Panel_lateral.png').convert_alpha()
 
 clock = pg.time.Clock()
 run = True
@@ -19,7 +18,6 @@ while run:
     screen.fill((100, 100, 100))
 
     screen.blit(piso1, (c.FORM_WIDTH, 0))
-    screen.blit(panel, (0, 0))
 
     for event in pg.event.get():
         # cerrar el juego
